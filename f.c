5eef241137c1a60e9acade795e983d239c9feb85
acninwza <ahit_2001@outlook.co.th>
2021-01-12 15:26:30 +0700

    first commit

---
+++ f.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main(){
+    int num1, num2, total;
+    printf("Value of num1 is :");
+    scanf("%d" ,&num1);
+
+    printf("Value of num2 is :");
+    scanf("%d" ,&num2);
+
+    total = num1 + num2;
+    printf("%d",total);
+     
+    //Finish
+}
